58fdc3baef3de45d921449084f10b1fa7a4449a5
ocha <ochaochaocha3@gmail.com>
2019-07-03 22:00:01 +0900

    parser: エラーとなるテストケースを追加する

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -8,27 +8,45 @@ func TestParse(t *testing.T) {
 	testCases := []struct {
 		input        string
 		expectedSExp string
+		err          bool
 	}{
-		{"C(1)", "(Calc 1)"},
-		{"C(42)", "(Calc 42)"},
-		{"C(1+2)", "(Calc (+ 1 2))"},
-		{"C(1-2)", "(Calc (- 1 2))"},
-		{"C(1*2)", "(Calc (* 1 2))"},
-		{"C(1/2)", "(Calc (/ 1 2))"},
-		{"C(1+2-3)", "(Calc (- (+ 1 2) 3))"},
-		{"C(1*2+3)", "(Calc (+ (* 1 2) 3))"},
-		{"C(1/2+3)", "(Calc (+ (/ 1 2) 3))"},
-		{"C(1+2*3)", "(Calc (+ 1 (* 2 3)))"},
-		{"C(1+2/3)", "(Calc (+ 1 (/ 2 3)))"},
-		{"2D6", "(DRoll 2 6)"},
-		{"12D60", "(DRoll 12 60)"},
+		{"C(1)", "(Calc 1)", false},
+		{"C(42)", "(Calc 42)", false},
+		{"C(1+2)", "(Calc (+ 1 2))", false},
+		{"C(1-2)", "(Calc (- 1 2))", false},
+		{"C(1*2)", "(Calc (* 1 2))", false},
+		{"C(1/2)", "(Calc (/ 1 2))", false},
+		{"C(1+2-3)", "(Calc (- (+ 1 2) 3))", false},
+		{"C(1*2+3)", "(Calc (+ (* 1 2) 3))", false},
+		{"C(1/2+3)", "(Calc (+ (/ 1 2) 3))", false},
+		{"C(1+2*3)", "(Calc (+ 1 (* 2 3)))", false},
+		{"C(1+2/3)", "(Calc (+ 1 (/ 2 3)))", false},
+		{"CC(1)", "", true},
+		{"2D6", "(DRoll 2 6)", false},
+		{"2D6D6", "", true},
+		{"12D60", "(DRoll 12 60)", false},
 	}
 
 	for i, test := range testCases {
 		actual, err := Parse(test.input)
 
 		if err != nil {
-			t.Errorf("#%d (%q): got err: %s", i, test.input, err)
+			// エラーが発生した！
+
+			if !test.err {
+				// エラーを想定していなかった場合
+				t.Errorf("#%d (%q): got err: %s", i, test.input, err)
+			}
+
+			// エラーを想定していたので、この挙動は正常
+
+			continue
+		}
+
+		if test.err {
+			// エラーを想定していたのに、発生していない
+			t.Errorf("#%d (%q): should err", i, test.input)
+
 			continue
 		}
 
