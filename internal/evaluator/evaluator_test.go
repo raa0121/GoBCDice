6904d4fd85a5a4f21c799b01e3e377f5236bd21d
ocha <ochaochaocha3@gmail.com>
2019-07-05 17:46:02 +0900

    ダイスロール評価のテストを追加する
    [skip ci]

--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"github.com/raa0121/GoBCDice/internal/die"
 	"github.com/raa0121/GoBCDice/internal/object"
 	"github.com/raa0121/GoBCDice/internal/parser"
 	"testing"
@@ -39,6 +40,162 @@ func TestEvalCalc(t *testing.T) {
 		{"C((1+2)/3)", 1},
 		{"C(-(1+2))", -3},
 		{"C(+(1+2))", 3},
+	}
+
+	for i, test := range testcases {
+		input := test.input
+		ast, parseErr := parser.Parse(input)
+		if parseErr != nil {
+			t.Errorf("#%d: %q: 構文解析エラー: %s", i, input, parseErr)
+			continue
+		}
+
+		// ノードを評価する
+		evaluated, evalErr := Eval(ast)
+		if evalErr != nil {
+			t.Errorf("#%d: %q: 評価エラー: %s", i, input, evalErr)
+			continue
+		}
+
+		if evaluated == nil {
+			t.Errorf("#%d: %q: Evalの対象外 (nil)", i, input)
+			continue
+		}
+
+		// 型が合っているか？
+		obj, typeMatched := evaluated.(*object.Integer)
+		if !typeMatched {
+			t.Errorf("#%d: %q: 整数オブジェクトでない: got=%T (%+v)", i, input, obj, obj)
+			continue
+		}
+
+		if obj.Value != test.expected {
+			t.Errorf("#%d: %q: 異なる値: got=%d, want=%d", i, input, obj.Value, test.expected)
+		}
+	}
+}
+
+func TestEvalDRollExpr(t *testing.T) {
+	testcases := []struct {
+		input    string
+		expected int
+		dice     []die.Die
+	}{
+		{
+			input:    "2D6",
+			expected: 8,
+			dice:     []die.Die{{5, 6}, {3, 6}},
+		},
+		{
+			input:    "2D4",
+			expected: 3,
+			dice:     []die.Die{{1, 4}, {2, 4}},
+		},
+		{
+			input:    "2D6+1",
+			expected: 9,
+			dice:     []die.Die{{2, 6}, {6, 6}},
+		},
+		{
+			input:    "1+2D6",
+			expected: 8,
+			dice:     []die.Die{{4, 6}, {3, 6}},
+		},
+		{
+			input:    "2d6+1-1-2-3-4",
+			expected: -2,
+			dice:     []die.Die{{1, 6}, {6, 6}},
+		},
+		{
+			input:    "2D6+4D10",
+			expected: 30,
+			dice:     []die.Die{{5, 6}, {4, 6}, {1, 10}, {9, 10}, {7, 10}, {4, 10}},
+		},
+		{
+			input:    "2d6*3",
+			expected: 18,
+			dice:     []die.Die{{2, 6}, {4, 6}},
+		},
+		{
+			input:    "2d10+3-4",
+			expected: 7,
+			dice:     []die.Die{{3, 10}, {5, 10}},
+		},
+		{
+			input:    "2d10+3*4",
+			expected: 20,
+			dice:     []die.Die{{3, 10}, {5, 10}},
+		},
+		{
+			input:    "2d6*3-1d6+1",
+			expected: 22,
+			dice:     []die.Die{{6, 6}, {2, 6}, {3, 6}},
+		},
+		{
+			input:    "(2+3)d6-1+3d6+2",
+			expected: 31,
+			dice:     []die.Die{{2, 6}, {3, 6}, {1, 6}, {5, 6}, {6, 6}, {5, 6}, {4, 6}, {4, 6}},
+		},
+		{
+			input:    "(2*3-4)d6-1d4+1",
+			expected: 10,
+			dice:     []die.Die{{6, 6}, {5, 6}, {2, 6}},
+		},
+		{
+			input:    "((2+3)*4/3)d6*2+5",
+			expected: 53,
+			dice:     []die.Die{{6, 6}, {5, 6}, {6, 6}, {2, 6}, {1, 6}, {4, 6}},
+		},
+		{
+			input:    "1D6/2",
+			expected: 0,
+			dice:     []die.Die{{1, 6}},
+		},
+		{
+			input:    "3D6/2",
+			expected: 3,
+			dice:     []die.Die{{1, 6}, {2, 6}, {4, 6}},
+		},
+		{
+			input:    "3D6/2+1D6",
+			expected: 8,
+			dice:     []die.Die{{1, 6}, {2, 6}, {4, 6}, {5, 6}},
+		},
+		{
+			input:    "3D6+1D6/2",
+			expected: 9,
+			dice:     []die.Die{{1, 6}, {2, 6}, {4, 6}, {5, 6}},
+		},
+		{
+			input:    "3D6+1D6/2U",
+			expected: 0,
+			dice:     []die.Die{{1, 6}, {2, 6}, {4, 6}, {5, 6}},
+		},
+		{
+			input:    "5D6/10",
+			expected: 2,
+			dice:     []die.Die{{6, 6}, {6, 6}, {6, 6}, {6, 6}, {5, 6}},
+		},
+		{
+			input:    "3D6/2U",
+			expected: 4,
+			dice:     []die.Die{{1, 6}, {2, 6}, {4, 6}},
+		},
+		{
+			input:    "5D6/10u",
+			expected: 3,
+			dice:     []die.Die{{6, 6}, {6, 6}, {6, 6}, {2, 6}, {1, 6}},
+		},
+		{
+			input:    "1D100/10R",
+			expected: 6,
+			dice:     []die.Die{{55, 100}},
+		},
+		{
+			input:    "1D100/10r",
+			expected: 5,
+			dice:     []die.Die{{54, 100}},
+		},
 	}
 
 	for i, test := range testcases {
