9207f71909cf225ea8a8c49b78a446350d2d8ea2
ocha <ochaochaocha3@gmail.com>
2019-07-20 18:27:42 +0900

    REPL: go-colorableを使う
    refs #11
    
    Windowsのコマンドプロンプトでも正常に表示されるようにするため

--- cmd/GoBCDiceREPL/main.go
+++ cmd/GoBCDiceREPL/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/mattn/go-colorable"
 	"github.com/raa0121/GoBCDice/cmd/GoBCDiceREPL/repl"
 	"os"
 )
 
 func main() {
-	fmt.Println("\033[1mGoBCDice REPL\033[0m")
-	fmt.Println("\n* BCDiceコマンドを入力すると、その評価結果を出力します")
-	fmt.Println("* \".help\" と入力すると、利用できるコマンドの使用法と説明を出力します")
-	fmt.Println("* \".q\" または \".quit\" と入力すると終了します")
-	fmt.Println("")
+	out := colorable.NewColorableStdout()
 
-	r := repl.New(os.Stdin, os.Stdout)
+	fmt.Fprintln(out, "\033[1mGoBCDice REPL\033[0m")
+	fmt.Fprintln(out, "\n* BCDiceコマンドを入力すると、その評価結果を出力します")
+	fmt.Fprintln(out, "* \".help\" と入力すると、利用できるコマンドの使用法と説明を出力します")
+	fmt.Fprintln(out, "* \".q\" または \".quit\" と入力すると終了します")
+	fmt.Fprintln(out, "")
+
+	r := repl.New(os.Stdin, out)
 	r.Start()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/raa0121/GoBCDice
 require (
 	github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+	github.com/mattn/go-colorable v0.1.2
 	github.com/pkg/errors v0.8.1
 	github.com/remyoudompheng/bigfft v0.0.0-20190512091148-babf20351dd7 // indirect
 	github.com/seehuhn/mt19937 v0.0.0-20180715112136-cc7708819361
