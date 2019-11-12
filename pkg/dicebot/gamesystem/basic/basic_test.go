95b4c7fbfd67912fada640639b342990f631aadf
ocha <ochaochaocha3@gmail.com>
2019-11-13 01:15:08 +0900

    basic: テストを追加する
    GameID、GameName、Usageをカバーするようにする

--- pkg/dicebot/gamesystem/basic/basic_test.go
+++ pkg/dicebot/gamesystem/basic/basic_test.go
@@ -1,9 +1,11 @@
 package basic_test
 
 import (
-	dicebottesting "github.com/raa0121/GoBCDice/pkg/dicebot/testing"
 	"path/filepath"
 	"testing"
+
+	"github.com/raa0121/GoBCDice/pkg/dicebot/gamesystem/basic"
+	dicebottesting "github.com/raa0121/GoBCDice/pkg/dicebot/testing"
 )
 
 func TestDiceBot(t *testing.T) {
@@ -23,6 +25,30 @@ func TestDiceBot(t *testing.T) {
 
 	testDataFiles := joinWithTestData(testDataFileBaseNames)
 	dicebottesting.Run("DiceBot", t, testDataFiles...)
+}
+
+func TestBasic_GameID(t *testing.T) {
+	expected := "DiceBot"
+	actual := basic.New().GameID()
+
+	if actual != expected {
+		t.Fatalf("got: %q, want: %q", actual, expected)
+	}
+}
+
+func TestBasic_GameName(t *testing.T) {
+	expected := "ダイスボット (指定無し)"
+	actual := basic.New().GameName()
+
+	if actual != expected {
+		t.Fatalf("got: %q, want: %q", actual, expected)
+	}
+}
+
+func TestBasic_Usage(t *testing.T) {
+	if len(basic.New().Usage()) <= 0 {
+		t.Fatal("Usage() が空文字列")
+	}
 }
 
 func joinWithTestData(basenames []string) []string {
